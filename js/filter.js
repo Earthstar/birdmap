21644daec39198f1421b36ab8ec93a5103d3ab76
Deena Wang <thedeenawang@gmail.com>
2014-04-02 18:17:30 -0400

    Implements Filter.bySpecies

--- js/filter.js
+++ js/filter.js
@@ -3,6 +3,7 @@ var Filter = {
 
   // sightings - list of objects representing birds
   // start, end - Date objects
+  // Not a very efficient implementation.
   byDate: function(sightings, start, end) {
     var matches = [];
     for (var i = sightings.length - 1; i >= 0; i--) {
@@ -17,6 +18,15 @@ var Filter = {
 
   // species - string representing species. Case-insensitive
   bySpecies: function(sightings, species) {
+    var matches = [];
+    var capSpecies = species.toUpperCase();
+    for (var i = sightings.length - 1; i >= 0; i--) {
+      var sightingSpecies = sightings[i].speciesName;
+      if (sightingSpecies.toUpperCase() === capSpecies) {
+        matches.push(sightings[i])
+      }
+    };
+    return matches;
   },
 
   // user - string of user name. Case-sensitive
