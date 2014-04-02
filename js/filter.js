138c74482239e1d726b2fb8d3b88bdceab3ecdb0
Deena Wang <thedeenawang@gmail.com>
2014-04-02 18:04:25 -0400

    Adds loadBirds function, implements Filter.byDate, changes some single quotes to double quotes

--- js/filter.js
+++ js/filter.js
@@ -1,15 +1,21 @@
-// Should birds position object be processed into latlng?
 var Filter = function() {
 
   // sightings - list of objects representing birds
   // start, end - Date objects
   this.byDate = function(sightings, start, end) {
-
+    var matches = [];
+    for (var i = sightings.length - 1; i >= 0; i--) {
+      var sightingDate = sightings[i].date;
+      if ((sightingDate.getTime() >= start.getTime()) &&
+        sightingDate.getTime <= end.getTime()) {
+        matches.push(sightings[i]);
+      }
+    }
+    return matches;
   }
 
   // species - string representing species. Case-insensitive
   this.bySpecies = function(sightings, species) {
-
   }
 
   // user - string of user name. Case-sensitive
