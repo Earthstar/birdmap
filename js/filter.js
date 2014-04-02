744e0937f409d1d071750e2293c762312e4d304c
Deena Wang <thedeenawang@gmail.com>
2014-04-02 18:11:59 -0400

    Fix bugs in filter

--- js/filter.js
+++ js/filter.js
@@ -1,30 +1,31 @@
-var Filter = function() {
+// Reserve Filter namespace
+var Filter = {
 
   // sightings - list of objects representing birds
   // start, end - Date objects
-  this.byDate = function(sightings, start, end) {
+  byDate: function(sightings, start, end) {
     var matches = [];
     for (var i = sightings.length - 1; i >= 0; i--) {
       var sightingDate = sightings[i].date;
       if ((sightingDate.getTime() >= start.getTime()) &&
-        sightingDate.getTime <= end.getTime()) {
+        sightingDate.getTime() <= end.getTime()) {
         matches.push(sightings[i]);
       }
     }
     return matches;
-  }
+  },
 
   // species - string representing species. Case-insensitive
-  this.bySpecies = function(sightings, species) {
-  }
+  bySpecies: function(sightings, species) {
+  },
 
   // user - string of user name. Case-sensitive
-  this.byUser = function(sightings, user) {
+  byUser: function(sightings, user) {
 
-  }
+  },
 
   // bounds - a google.maps.LatLngBounds object representing area
-  this.byLocation = function(sightings, bounds) {
+  byLocation: function(sightings, bounds) {
 
-  }
+  },
 }
