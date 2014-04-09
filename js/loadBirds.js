d827bb5ff3b3d17038ce0bdf72511879bd83b689
Deena Wang <thedeenawang@gmail.com>
2014-04-09 12:11:07 -0400

    Adds bird popup to map

--- js/loadBirds.js
+++ js/loadBirds.js
@@ -13,23 +13,52 @@
 }
 */
 // Json is already parsed
-function processBirds(json) {
-  var sightings = [];
-  for (var i = json.length - 1; i >= 0; i--) {
-    var sighting = json[i];
-    var position = new google.maps.LatLng(sighting.position.latitude,
-      sighting.position.longitude);
-    var date = new Date(sighting.date);
-    sighting.position = position;
-    sighting.date = date;
-    sightings.push(sighting)
-  };
-  return sightings
-}
+$(function() {
 
-var birdSightings;
+  function processBirds(json) {
+    var sightings = [];
+    for (var i = json.length - 1; i >= 0; i--) {
+      var sighting = json[i];
+      var position = new google.maps.LatLng(sighting.position.latitude,
+        sighting.position.longitude);
+      var date = new Date(sighting.date);
+      sighting.position = position;
+      sighting.date = date;
+      sightings.push(sighting)
+    };
+    return sightings
+  }
 
-$.getJSON('birds.json', function(json) {
-  birdSightings = processBirds(json);
-  console.log(birdSightings);
+  // Given a sighting object, displays on map
+  function displayBird(sighting) {
+    console.log("in display bird")
+    var source = $("#bird-info-popup-template").html();
+    var popupTemplate = Handlebars.compile(source);
+    var html = popupTemplate(sighting)
+    console.log(html)
+    var infoWindow = new google.maps.InfoWindow({
+      content: html
+    })
+    var position = sighting.position;
+    console.log(position)
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: sighting.commonName
+    })
+    google.maps.event.addListener(marker, "click", function() {
+      infoWindow.open(map, marker)
+    })
+    console.log("after display bird")
+  }
+
+  var birdSightings;
+
+  $.getJSON('birds.json', function(json) {
+    birdSightings = processBirds(json);
+    console.log(birdSightings);
+    // Find all birds that are on the screen
+    // display sightings
+    displayBird(birdSightings[0])
+  })
 })
