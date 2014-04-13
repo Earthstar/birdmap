a9981e3c4753057e2fcfbc3e6602089e45a3505a
Deena Wang <thedeenawang@gmail.com>
2014-04-13 15:03:41 -0400

    Refactors Display.birdSightings function

--- js/utilities.js
+++ js/utilities.js
@@ -57,7 +57,7 @@ var Birdmap = {
       sighting.date = new Date(Date.parse(datestring));
       sightings.push(sighting);
     };
-    return sightings
+    return sightings;
   }
 }
 
@@ -125,27 +125,23 @@ var Display = {
     this.popupTemplate = Handlebars.compile(source);
   },
 
-  // Given a sighting object, displays on map
-  birdSighting: function(sighting) {
-    var html = this.popupTemplate(sighting)
-    var infoWindow = new google.maps.InfoWindow({
-      content: html
-    })
-    var position = sighting.position;
-    var marker = new google.maps.Marker({
-      position: position,
-      map: map,
-      title: sighting.commonName
-    })
-    google.maps.event.addListener(marker, "click", function() {
-      infoWindow.open(map, marker)
-    })
-  },
-
   // Given a list of sighting objects, displays them on map
   birdSightings: function(sightings) {
     for (var i = sightings.length - 1; i >= 0; i--) {
-      Display.birdSighting(sightings[i])
+      var sighting = sightings[i]
+      var html = this.popupTemplate(sighting)
+      var infoWindow = new google.maps.InfoWindow({
+        content: html
+      })
+      var position = sighting.position;
+      var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: sighting.commonName
+      })
+      google.maps.event.addListener(marker, "click", function() {
+        infoWindow.open(map, marker)
+      })
     };
   }
 }
