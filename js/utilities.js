015a41516ff7563cd835f935c2d5834b7c05f38a
lilisun <lilis@mit.edu>
2014-04-15 16:55:56 -0400

    list of birds populated

--- js/utilities.js
+++ js/utilities.js
@@ -142,11 +142,11 @@ var Display = {
     })
     markers.push(marker);
   },
-
+    
   // Given a list of sighting objects, displays them on map
   birdSightings: function(sightings) {
     for (var i = sightings.length - 1; i >= 0; i--) {
-      Display.birdSighting(sightings[i])
+      Display.birdSighting(sightings[i]);
     };
   }
 }
